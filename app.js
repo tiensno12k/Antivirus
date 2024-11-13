7816510a11a069af47ccb75893580b6bf60f29be
Tien Tran <tranqtiennd.2005@gmail.com>
2024-11-13 20:14:48 +0700

    Update

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ document.querySelector('.box-navbar i').addEventListener("click", toggleContent)
 
 function toggleContent() {
     var boxWrap = document.querySelector('.box-wrap2')
-    if (
-        boxWrap.style.display === "none") {
-            boxWrap.style.display = "block";
-        } else {
-            
-            boxWrap.style.display = "none";
-        }
+    if (boxWrap.style.display === "none") {
+        boxWrap.style.display = "block";
     }
+    else {
+        boxWrap.style.display = "none";
+    }
+
+}
